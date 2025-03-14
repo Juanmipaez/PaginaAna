1bd843ca6b80498bc26616e43e9be7433e6128cd
Juanmipaez13 <85180095+Juanmipaez@users.noreply.github.com>
2025-03-14 11:19:08 -0500

    Delete Pagina_web_Jef directory

--- Pagina_web_Jef/Pagina_inicial/functions.js
+++
@@ -1,8 +0,0 @@
-const htmlbody = document.querySelector('body');
-
-const clickfunction = function(){
-    const colors=['red', 'blue', 'purple', 'green'];
-    const randomIndex = Math.floor(Math.random() * colors.length );
-    const randomColor = colors[randomIndex];
-    htmlbody.style.backgroundColor = randomColor;
-}
